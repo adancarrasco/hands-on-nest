ecd72fa92a5b4b0d1a4b8a64649350339c1f42ce
Adán Carrasco <adan.carrasco@hotmail.com>
2020-04-19 02:12:42 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+@Module({
+  imports: [UsersModule, ProductsModule],
+})
+export class AppModule {}
